93ed8c7e386489fa10d5e224fed6c0ec729518ed
Smafair <shanefairweather@gmail.com>
2017-05-13 01:45:38 +0300

    c

--- src/components/brandBar.js
+++ src/components/brandBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import lacoste from '../../assets/img/lacoste-logo.png';
 import nike from '../../assets/img/nike.png';
-import puma from '../../assets/img/123.png';
+import tommy from '../../assets/img/123.png';
 import adidas from '../../assets/img/580b57fcd9996e24bc43c4f2.png';
-import tommy from '../../assets/img/lacoste-logo.png';
+import puma from '../../assets/img/Puma-Logo-1967-2010.jpg';
 import mango from '../../assets/img/mango_logo_detail.png';
 
 const BrandBar = (props) => {
@@ -13,10 +13,10 @@ const BrandBar = (props) => {
             <Row className="brandBar">
                 <Col xs={12} md={2}><img src={nike} /></Col>
                 <Col xs={12} md={2}><img src={lacoste} /></Col>
-                <Col xs={12} md={2}><img src={puma} /></Col>
+                <Col xs={12} md={2}><img id='tommy' src={tommy} /></Col>
                 <Col xs={12} md={2}><img src={adidas} /></Col>
-                <Col xs={12} md={2}><img src={mango} /></Col>
-                <Col xs={12} md={2}><img src={tommy} /></Col>
+                <Col xs={12} md={2}><img id='mango' src={mango} /></Col>
+                <Col xs={12} md={2}><img id='puma' src={puma} /></Col>
             </Row>
         )
     } else {
